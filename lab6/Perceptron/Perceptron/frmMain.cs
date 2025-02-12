526a4a92d5ac3352a2cdafac0299cf875365d8df
Jack Foley <jackfdevelopment@gmail.com>
2025-02-12 16:46:22 +0000

    Complete lab 6

---
+++ lab6/Perceptron/Perceptron/frmMain.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics; // For Debug.
+
+
+namespace Perceptron
+{
+    public partial class frmMain : Form
+    {
+        List<Sample> samples = new List<Sample>();
+        Graphics objGraphics;
+        Graphics grph;
+        double w1, w2, w0 = 0;
+        double eta = 0.2;
+        double x0 = 1; // Bias input will always be 1. Otherwise a bias weight w0 of 0 would never increase.
+
+        int maxIterations;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void picCanvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            objGraphics = pnlCanvas.CreateGraphics();
+            grph = pnlCanvas.CreateGraphics();
+        }
+
+        private void btnLearn_Click(object sender, EventArgs e)
+        {
+            int iterations = 0;
+            bool error = true;
+
+            maxIterations = int.Parse(txtIterations.Text);
+            eta = (double)trackLearningRate.Value / 1000;
+
+            w0 = 0;
+            w1 = 1;
+            w2 = 0.5;
+
+            while (error && iterations < maxIterations)
+            {
+                error = false;
+                iterations++;
+
+                foreach (Sample sample in samples)
+                {
+                    // Calculate output for current sample using current weights
+                    double sum = w0 * x0 + w1 * sample.X1 + w2 * sample.X2;
+                    int output = sum >= 0 ? 1 : -1;
+
+                    // Check if there's an error
+                    if (output != sample.Class)
+                    {
+                        // Update weights using the Perceptron Learning Rule
+                        //   w = w + eta * (d - y) * x
+                        w0 = w0 + eta * (sample.Class - output) * x0;
+                        w1 = w1 + eta * (sample.Class - output) * sample.X1;
+                        w2 = w2 + eta * (sample.Class - output) * sample.X2;
+                        error = true;
+                    }
+                }
+
+                // Redraw the separation line to show progress
+                objGraphics.Clear(Color.White);
+                DrawSeparationLine();
+                Console.WriteLine($"Iteration: {iterations} - Weights: w0={w0:F3} w1={w1:F3} w2={w2:F3}");
+                Application.DoEvents();
+            }
+        }
+
+        private void picCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            double posX = (double)(e.X - pnlCanvas.Width / 2) / 10;
+            double posY = (double)(pnlCanvas.Height / 2 - e.Y) / 10;
+
+            this.Text = posX + " " + posY;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            samples.Clear();
+            objGraphics.Clear(Color.White);
+            objGraphics.DrawLine(new Pen(Color.Gainsboro), new Point(0, pnlCanvas.Height / 2), new Point(pnlCanvas.Width, pnlCanvas.Height / 2));
+            objGraphics.DrawLine(new Pen(Color.Gainsboro), new Point(pnlCanvas.Width / 2, 0), new Point(pnlCanvas.Width / 2, pnlCanvas.Height));
+        }
+
+        private void DrawSamples()
+        {
+            foreach (Sample sample in samples)
+            {
+
+                double posX = (pnlCanvas.Width / 2) + sample.X1 * 10;
+                double posY = (pnlCanvas.Height / 2) - sample.X2 * 10;
+
+                Pen pen;
+                if (sample.Class == 1)
+                {
+                    pen = new Pen(Color.Blue);
+                }
+                else
+                {
+                    pen = new Pen(Color.Red);
+                }
+
+                objGraphics.DrawLine(pen, new Point((int)posX - 3, (int)posY), new Point((int)posX + 3, (int)posY));
+                objGraphics.DrawLine(pen, new Point((int)posX, (int)posY - 3), new Point((int)posX, (int)posY + 3));
+            }
+        }
+
+        private void DrawSeparationLine()
+        {
+            objGraphics.DrawLine(new Pen(Color.Gainsboro), new Point(0, pnlCanvas.Height / 2), new Point(pnlCanvas.Width, pnlCanvas.Height / 2));
+            objGraphics.DrawLine(new Pen(Color.Gainsboro), new Point(pnlCanvas.Width / 2, 0), new Point(pnlCanvas.Width / 2, pnlCanvas.Height));
+
+            Pen pen = new Pen(Color.DarkGreen);
+            double x1;
+
+            x1 = -10;   //Oisin: Just picks a random point to draw a line from using the weights.
+            double y = -(x1 * w1 / w2) - ((x0*w0) / w2);
+
+            double shift = pnlCanvas.Height / 2;
+
+
+            Point p1 = new Point(0, (int)(shift - y * 10));
+
+            x1 = 10;
+            double y2 = -(x1 * w1 / w2) - ((w0) / w2);
+
+            Point p2 = new Point(pnlCanvas.Width, (int)(shift - y2 * 10));
+            if (w2 != 0)
+            {
+                objGraphics.DrawLine(pen, p1, p2);
+                DrawSamples();
+            }
+        }
+
+        private void trackLearningRate_Scroll(object sender, EventArgs e)
+        {
+            lblLearningRate.Text = "Learning Rate: " + (double)trackLearningRate.Value/1000;
+        }
+
+        private void pnlCanvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            Sample sample;
+            Pen pen;
+
+            double posX = (double)(e.X - pnlCanvas.Width / 2) / 10;
+            double posY = (double)(pnlCanvas.Height / 2 - e.Y) / 10;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                pen = new Pen(Color.Blue);
+                sample = new Sample(posX, posY, 1);
+            }
+            else
+            {
+                pen = new Pen(Color.Red);
+                sample = new Sample(posX, posY, -1);
+            }
+            samples.Add(sample);
+
+            objGraphics.DrawLine(pen, new Point(e.X - 3, e.Y), new Point(e.X + 3, e.Y));
+            objGraphics.DrawLine(pen, new Point(e.X, e.Y - 3), new Point(e.X, e.Y + 3));
+        }
+
+        private void pnlCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            objGraphics.DrawLine(new Pen(Color.Gainsboro), new Point(0, pnlCanvas.Height / 2), new Point(pnlCanvas.Width, pnlCanvas.Height / 2));
+            objGraphics.DrawLine(new Pen(Color.Gainsboro), new Point(pnlCanvas.Width / 2, 0), new Point(pnlCanvas.Width / 2, pnlCanvas.Height));
+        }
+
+
+        
+    }
+}
